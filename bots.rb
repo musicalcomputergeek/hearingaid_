8d7b18eea2ef2f8e8aed67fce1db8c417a8bd7c2
Timothy <Timothy@Timothys-iMac.local>
2014-06-30 02:55:13 -0400

    more fixes, cleaning up
    I really really really need help with figuring out how to retrieve the
    original tweet to which the “what?” tweet was replying. The text from
    the original tweet is what needs to be tweeted in .upcase

--- bots.rb
+++ bots.rb
@@ -14,31 +14,21 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = keys['access_token_secret']
 end
 
-client.user("hearingaid_")
-
-  bot.on_mention do |tweet, meta|
-    # Reply to a mention
-    begin
-      bot.reply(tweet, meta[:reply_prefix] + "What?")
-    rescue
-      bot.reply(dm, "What? #{rand)(9999999)}")
-    end
-  end
-
   MAX_ATTEMPTS = 3
   num_attempts = 0
   follower_ids = client.follower_ids
 
-  client.search("what?", "what", "wut?", "wut", "wat?", "wat" :result_type => "recent") do |tweet|
+  client.search("what?", :result_type => "recent").take().each do |tweet|
     begin
       # counter and stuff for rate limit protection
       num_attempts += 1
       follower_ids.to_a
-      if meta[:reply_prefix].to_s.include? '@hearingaid_'
-        # do nothing
-      else
-        client.update(.upcase, :in_reply_to_status)
-      end
+      # SCREAM AT THEM
+      # *** the following must be updated to tweet.upcase the text /
+      # *** from the tweet the "what?" replied to
+      # *** any help with this is *extremely* appreciated
+      client.update(tweet.upcase, :in_reply_to_status)
+      client.search.clear
     rescue Twitter::Error::TooManyRequests => error
       if num_attempts <= MAX_ATTEMPTS
         # NOTE: Your process could go to sleep for up to 15 minutes but if you
@@ -48,11 +38,5 @@ client.user("hearingaid_")
       else
         raise
     end
-  end
-
-  bot.scheduler.every '24h' do
-    # Tweet something every 24 hours
-    # See https://github.com/jmettraux/rufus-scheduler
-    # bot.tweet("hi")
   end
 end
