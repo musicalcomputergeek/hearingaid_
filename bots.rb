aa5872f1f371c9491ac2d033142fdbdec8edb05b
Timothy <Timothy@Timothys-iMac.local>
2014-06-30 02:17:27 -0400

    rewritten for twitter gem, vs twitter_ebooks gem
    vastly different, with changed syntax and hidden keys and tokens
    (because of YOU)

--- bots.rb
+++ bots.rb
@@ -1,32 +1,20 @@
 #!/usr/bin/env ruby
+require 'rubygems'
+require 'yaml'
+require 'twitter'
+# ya know becuz u need dat stuff
 
-require 'twitter_ebooks'
+# keep stuff secret
+keys = YAML::load(File.open('keys.yml'))
 
-# This is an example bot definition with event handlers commented out
-# You can define as many of these as you like; they will run simultaneously
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = keys['consumer_key']
+  config.consumer_secret     = keys['consumer_secret']
+  config.access_token        = keys['access_token']
+  config.access_token_secret = keys['access_token_secret']
+end
 
-Ebooks::Bot.new("hearingaid_") do |bot|
-  # Consumer details come from registering an app at https://dev.twitter.com/
-  # OAuth details can be fetched with https://github.com/marcel/twurl
-  bot.consumer_key = "TcSWsro3AiTg0F3UtlS34hxMe" # Your app consumer key
-  bot.consumer_secret = "xhPCtSSAnKcQUbU6uTpVHHyCu3tfGQM6T5ml2Ji62buwuHw5sY" # Your app consumer secret
-  bot.oauth_token = "2593732968-zkeBcsLRT5oI0y5YI9YTGhwOgN8V3OgmwDbLjKC" # Token connecting the app to this account
-  bot.oauth_token_secret = "VFrAMNWh0tcjniEgnkKy7uh0w0Za98HKhJbsPdUoon4mK" # Secret connecting the app to this account
-
-  bot.on_message do |dm|
-    # Reply to a DM
-    begin
-      bot.reply(dm, "What?")
-    rescue
-      bot.reply(dm, "What? #{rand)(9999999)}")
-    end
-  end
-
-  bot.on_follow do |user|
-    # Follow a user back
-    # Not following back atm
-    # bot.follow(user[:screen_name])
-  end
+client.user("hearingaid_")
 
   bot.on_mention do |tweet, meta|
     # Reply to a mention
@@ -37,21 +25,28 @@ Ebooks::Bot.new("hearingaid_") do |bot|
     end
   end
 
-  bot.on_timeline do |tweet, meta|
-    # Reply to a tweet in the bot's timeline
-    # No replies since no follows
-    # bot.reply(tweet, meta[:reply_prefix] + "nice tweet")
-  end
+  MAX_ATTEMPTS = 3
+  num_attempts = 0
+  follower_ids = client.follower_ids
 
-  bot.twitter.client.search("what?", "what", "wut?", "wut", "wat?", "wat" :result_type => "recent") do |tweet|
+  client.search("what?", "what", "wut?", "wut", "wat?", "wat" :result_type => "recent") do |tweet|
     begin
+      # counter and stuff for rate limit protection
+      num_attempts += 1
+      follower_ids.to_a
       if meta[:reply_prefix].to_s.include? '@hearingaid_'
         # do nothing
       else
-        bot.reply(tweet, meta[:reply_prefix] + meta[:in_reply_to_status].upcase)
+        client.update(.upcase, :in_reply_to_status)
       end
-    rescue
-      # do nothing
+    rescue Twitter::Error::TooManyRequests => error
+      if num_attempts <= MAX_ATTEMPTS
+        # NOTE: Your process could go to sleep for up to 15 minutes but if you
+        # retry any sooner, it will almost certainly fail with the same exception.
+        sleep error.rate_limit.reset_in
+        retry
+      else
+        raise
     end
   end
 
